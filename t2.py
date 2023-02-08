44646f0e7d5ab542d3ec29cb78c48f2e808d7f30
Anusha <xyz@gmail.com>
2023-02-08 19:48:14 +0530

    loops.ipynb

---
+++ t2.py
@@ -0,0 +1,15 @@
+#turtle is python graphic library
+from turtle import *
+speed('slow')
+width(8)
+left(72)
+forward(120)
+left(72)
+forward(120)
+left(72)
+forward(120)
+left(72)
+forward(120)
+left(72)
+forward(120)
+mainloop() #this is to stop window from closing.
---
+++ t3.py
@@ -0,0 +1,12 @@
+#the power of not repeating
+from turtle import *
+
+side = 6
+for i in range(side):
+    pencolor('blue')
+    fd(100)
+    lt(360/side)
+    pencolor('red')
+    dot(30)
+
+mainloop()
